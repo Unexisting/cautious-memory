95626e8a551ac5f6c94ea764a4dd2edee18cd729
Unexisting <bumbac.cristy@gmail.com>
2024-03-07 16:18:25 +0000

    iterations.py

---
+++ iterations.py
@@ -0,0 +1,7 @@
+print("How many apples should i remove")
+apples = int(input())
+print("you entered: ", apples)
+removed_apples = 0
+while removed_apples < apples:
+    print("removed_apples. ")
+    removed_apples = removed_apples+1
